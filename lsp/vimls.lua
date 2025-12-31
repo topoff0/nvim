e42bb310febfca6a28759e640cb1fb01a34fc245
sweeppy <104524272+sweeppy@users.noreply.github.com>
2025-12-31 16:57:05 +0300

    Add mason and update lsp

--- lsp/vimls.lua
+++ lsp/vimls.lua
@@ -7,21 +7,20 @@ return {
 
   init_options = {
     diagnostic = {
-      enable = true
+      enable = true,
     },
     indexes = {
       count = 3,
       gap = 100,
       projectRootPatterns = { "runtime", "nvim", ".git", "autoload", "plugin" },
-      runtimepath = true
+      runtimepath = true,
     },
     isNeovim = true,
     iskeyword = "@,48-57,_,192-255,-#",
-    runtimepath = "",
     suggest = {
       fromRuntimepath = true,
-      fromVimruntime = true
+      fromVimruntime = true,
     },
-    vimruntime = ""
-  }
+  },
 }
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,5 +9,17 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Lines Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Lines Up" })
 
+-- Keep search results centered
+vim.keymap.set("n", "n", "nzzv", { desc = "Next Match (centered)" })
+vim.keymap.set("n", "N", "Nzzv", { desc = "Prev Match (centered)" })
+vim.keymap.set("n", "*", "*zzv", { desc = "Search Word (centered)" })
+vim.keymap.set("n", "#", "#zzv", { desc = "Search Word Back (centered)" })
+vim.keymap.set("n", "g*", "g*zz", { desc = "Search Partial (centered)" })
+vim.keymap.set("n", "g#", "g#zz", { desc = "Search Partial Back (centered)" })
+
+-- Clear search highlight
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { desc = "Clear Highlight", silent = true })
