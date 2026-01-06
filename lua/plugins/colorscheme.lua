4220f4ee96154e7695ada59a8b15d09c7306268f
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-01-06 18:11:21 +0300

    Update some vizual

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -31,7 +31,7 @@ return {
           underline_visible = false,
         },
         indent_blankline = {
-          context_highlight = "default", -- default | pro
+          context_highlight = "pro", -- default | pro
           context_start_underline = false,
         },
       },
