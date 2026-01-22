06632ab88075ba8caa5eacc7c16bee828a78e930
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-01-22 16:00:21 +0300

    Update shiftwidth for C# files and add blankline plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,9 +10,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd("Filetype", {
   pattern = "cs",
   callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
     vim.opt_local.expandtab = true
   end
 })
