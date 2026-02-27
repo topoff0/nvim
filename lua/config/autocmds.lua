c92d94bb57738552dd92d07631796d944d2e9073
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-02-27 17:43:14 +0300

    Add configuration for tabs in Haskell

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,8 +7,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- C# configuration tab configuration
-vim.api.nvim_create_autocmd("Filetype", {
+vim.api.nvim_create_autocmd("FileType", {
   pattern = "cs",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end
+})
+
+-- Haskell configuration tab configuration
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "haskell",
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
